9e36ac01da5c6a7feb89e6fb75a8aa3bd641e27a
bhaktibj <bhaktibj402@gmail.com>
2020-03-25 18:57:42 +0530

    adding user test cases

--- tests/fundooNotes/services/test_user_api.py
+++ tests/fundooNotes/services/test_user_api.py
@@ -1,26 +1,30 @@
 import requests
-from setting import FUNDOO
+from fundoonote.setting import FUNDOO
 base_url = FUNDOO['base_url']
 
 
 class TestCase:
+    """ This test class is used test all user test cases"""
 
     def test_register_valid(self):
         url = base_url + '/register'
-        data = {'username': 'admin20', 'password': 'admin20@123', 'email': 'admin21@gmail.com'}
+        data = {'first_name': 'admin','last_name':'admin20', 'email': 'admin2gmail.com'}
         res = requests.post(url=url, data=data)
+        print(res.text)
         assert res.status_code==200
 
     def test_register_invalid(self):
         url = base_url + '/register'
-        data = {'username': 'admin20', 'password': 'admin20@123', 'email': 'admingmail.com'}
+        data = {'first_name': 'admin20','last_name':'admin@123', 'password':'', 'email': 'admingmail.com'}
         res = requests.post(url=url, data=data)
+        print(res.text)
         assert res.status_code
 
     def test_login_valid(self):
         url = base_url + '/login'
-        data = {'email': 'admin16@gmail.com', 'password': 'admin16@123'}
+        data = {'email': 'nikita.pawar005@gmail.com', 'password': 'nikita123'}
         res = requests.post(url, data=data)
+        print(res.text)
         assert res.status_code==200
 
     def test_login_invalid(self):
@@ -31,7 +35,8 @@ class TestCase:
 
     def test_forgot(self):
         url = base_url + '/forgot'
-        data = {'email': 'admin16@gmail.com'}
-        res = requests.post(url, data=data)
+        headers = {'token': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwiZXhwIjoxNTg0OTc1MzU4fQ.S0T0A8dozmD2DSHEqqwlcGdrt_kMNBLXfPHEWf6BT_0'}
+        data = {'email': 'nikita.pawar005@gmail.com'}
+        res = requests.post(url, data=data, headers=headers)
         assert res.status_code ==200
 
